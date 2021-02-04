9595fbfbe6156880b262e5430144e3483cdf3991
Marcus Becker <marcus.becker@tudelft.nl>
2021-02-04 14:46:52 +0100

    New and hopefully final version of structs
    The new strategy is to create Ensemble structs with a turbine struct array & a constants struct. Each turbine element owns its OPs and their states. Further it has its location, turbine states and the turbine type. The turbine type links to the constants where the Diameter, Efficiency and p_p for said turbine are stored. In there also the chain constants are stored

--- Code/FLORIDynStructs.jl
+++ Code/FLORIDynStructs.jl
@@ -1,101 +1,51 @@
 # Structs for FLORIDyn
 module FLORIDyn_Structs
-export OP, T, E, Sim, Control
-# OPs
-struct OP
-    # World coordinates
-    x
-    y
-    z
+export Ensemble, Turbine
 
-    # Wake coordinates
-    x1  # Down wind
-    y1  # Cross wind
-    # z1 == z  # height
+# Ensembles contain simulation variables
 
-    # Wind Field states
-    u   # Wind speed
-    ϕ   # Wind direction
-    i0  # Ambient turbulence intensity
+struct Ensemble
+    turb        # Turbines
+    constants   # turbine and chain constants
 end
 
-struct C
-    c_w     # Weights of the chain / relative area
-    c_νy    # Relative y position in wake coordinates
-    c_νz    # Relative z position in wake coordinates
-
-    # Pointer to the chain start in the OP arrays
-    #   E1:
-    #       t1:
-    #           c1->OP1
-    #           c2->OP1
-    #           c2->OP1
-    #       t2:
-    #           c1->OP1
-    #           c2->OP1
-    #           c2->OP1
-    #   E2:
-    #       t1:
-    #           c1->OP1
-    #           c2->OP1
-    #           c2->OP1
-    #       t2:
-    #           c1->OP1
-    #           c2->OP1
-    #           c2->OP1
-    pntr_c2op 
+struct Constants
+    # Turbine constants
+    t_D
+    t_η
+    t_p_p
+    # Chain constants
+    c_w
+    c_νy
+    c_νz
 end
-# Turbines
-struct T
+
+struct Turbine
+    # TURBINE
     # Constants
     x   # World coordinate x
     y   # World coordinate y
     z   # Hub height
-    D   # Diameter
-    η   # Efficiency
-    p_p # Power exponent
+
+    ttype # Turbine type
 
     # States
     Ct  # Thrust coefficient
     γ   # yaw angle
     Cp  # Power coefficient
 
-    # Pointer to chain
-    #   E1:
-    #       t1->c1
-    #       t2->c1
-    #       t3->c1
-    #   E2:
-    #       t1->c1
-    #       t2->c1
-    #       t3->c1
-    pntr_t2c
-end
-
-struct E
-     # Pointer from the ensembles to the first turbine of the ensemble
-     # E1 -> T1,C1
-     # E2 -> T1,C1
-    pntr_e2c
-end
-# Simulation data
-struct Sim
-    # Time
-    t_end   # Duration
-    Δt      # Timestep duration
-    nt      # Number of timesteps
-
-    # Gaussian Wake Model
-    w       # WidthFactor
-
-    # Environment
-    ρ       # Air density
-    α_s     # Shear coefficient (Atmospheric stability, Power law)
-end
-
-struct Control
-    type :: String  # Name of control strategy
-    init :: Bool    # Initialize values or not
+    # OBSERVATION POINTS
+    # World coordinates
+    op_x    # [nOP x nC]
+    op_y    # [nOP x nC]
+    op_z    # [nOP x nC]
+    # Wake coordinates
+    op_x1   # [nOP x nC]
+    op_y1   # [nOP x nC]
+    # Wind field states coordinates
+    op_u    # [nOP x nC]
+    op_ϕ    # [nOP x nC]
+    op_I0   # [nOP x nC]
 end
 
 end  # module FLORIDyn_Structs
--- Code/InitStructs.jl
+++ Code/InitStructs.jl
@@ -20,167 +20,83 @@ lyt_9T    = [600.0,600.0,1500.0,600.0,2400.0,
             600.0,1500.0,1500.0,1500.0,2400.0,1500.0,
             600.0,2400.0,1500.0,2400.0,2400.0,2400.0];
 # Turbine data (z,D,η,p_p)
-DTU10MW = [119.0, 178.3, 1.08, 1.5]
+DTU10MW = [119.0, 178.4, 1.08, 1.5]
+InnWind = [119.0, 178.3, 1.08, 1.5]
 
-
-function allocTurbineStruct(layout::String,nE::Integer,nC::Integer,nOP::Integer)
-    if isequal(layout,"1T_FarmConners")
-        nT = 1;
-        t = T(
-            lyt_FC_1T[1],       # x
-            lyt_FC_1T[2],       # y
-            DTU10MW[1],         # z/HubHeight
-            DTU10MW[2],         # D
-            DTU10MW[3],         # η
-            DTU10MW[4],         # p_p
-            zeros(nOP*nT),      # Ct
-            zeros(nOP*nT),      # γ
-            zeros(nT),          # Cp
-            # Pointer to first chain of each turbine
-            1:nC:(nE*nT*nC-nC+1));
-        return t
-    elseif isequal(layout,"3T_FarmConners")
-        nT = 3;
-        t = T(
-            lyt_FC_3T[1:2:end],     # x
-            lyt_FC_3T[2:2:end],     # y
-            ones(nT).*DTU10MW[1],   # z/HubHeight
-            ones(nT).*DTU10MW[2],   # D
-            ones(nT).*DTU10MW[3],   # η
-            ones(nT).*DTU10MW[4],   # p_p
-            zeros(nOP*nT),          # Ct
-            zeros(nOP*nT),          # γ
-            zeros(nT),              # Cp
-            # Pointer to first chain of each turbine
-            1:nC:(nE*nT*nC-nC+1));
-        return t
-    elseif isequal(layout,"9T_FarmConner")
-        nT = 9;
-        t = T(
-            lyt_FC_9T[1:2:end],     # x
-            lyt_FC_9T[2:2:end],     # y
-            ones(nT).*DTU10MW[1],   # z/HubHeight
-            ones(nT).*DTU10MW[2],   # D
-            ones(nT).*DTU10MW[3],   # η
-            ones(nT).*DTU10MW[4],   # p_p
-            zeros(nOP*nT),          # Ct
-            zeros(nOP*nT),          # γ
-            zeros(nT),              # Cp
-            # Pointer to first chain of each turbine
-            1:nC:(nE*nT*nC-nC+1));
-        return t
-    elseif isequal(layout,"1T_DTU10MW")
-        nT = 2;
-        t = T(
-            lyt_2T[1:2:end],        # x
-            lyt_2T[2:2:end],        # y
-            ones(nT).*DTU10MW[1],   # z/HubHeight
-            ones(nT).*DTU10MW[2],   # D
-            ones(nT).*DTU10MW[3],   # η
-            ones(nT).*DTU10MW[4],   # p_p
-            zeros(nOP*nT),          # Ct
-            zeros(nOP*nT),          # γ
-            zeros(nT),              # Cp
-            # Pointer to first chain of each turbine
-            1:nC:(nE*nT*nC-nC+1));
-        return t
-    elseif isequal(layout,"2T_DTU10MW")
-        nT = 2;
-        t = T(
-            lyt_2T[1:2:end],        # x
-            lyt_2T[2:2:end],        # y
-            ones(nT).*DTU10MW[1],   # z/HubHeight
-            ones(nT).*DTU10MW[2],   # D
-            ones(nT).*DTU10MW[3],   # η
-            ones(nT).*DTU10MW[4],   # p_p
-            zeros(nOP*nT),          # Ct
-            zeros(nOP*nT),          # γ
-            zeros(nT),              # Cp
-            # Pointer to first chain of each turbine
-            1:nC:(nE*nT*nC-nC+1));
-        return t
-    elseif isequal(layout,"3T_DTU10MW")
-        nT = 3;
-        t = T(
-            lyt_3T[1:2:end],        # x
-            lyt_3T[2:2:end],        # y
-            ones(nT).*DTU10MW[1],   # z/HubHeight
-            ones(nT).*DTU10MW[2],   # D
-            ones(nT).*DTU10MW[3],   # η
-            ones(nT).*DTU10MW[4],   # p_p
-            zeros(nOP*nT),          # Ct
-            zeros(nOP*nT),          # γ
-            zeros(nT),              # Cp
-            # Pointer to first chain of each turbine
-            1:nC:(nE*nT*nC-nC+1));
-        return t
-    elseif isequal(layout,"9T_DTU10MW")
-        nT = 9;
-        t = T(
-            lyt_9T[1:2:end],        # x
-            lyt_9T[2:2:end],        # y
-            ones(nT).*DTU10MW[1],   # z/HubHeight
-            ones(nT).*DTU10MW[2],   # D
-            ones(nT).*DTU10MW[3],   # η
-            ones(nT).*DTU10MW[4],   # p_p
-            zeros(nOP*nT),          # Ct
-            zeros(nOP*nT),          # γ
-            zeros(nT),              # Cp
-            # Pointer to first chain of each turbine
-            1:nC:(nE*nT*nC-nC+1));
-        return t
+function InitEnsemble(layout,nC,nOP)
+    # Load layout
+    if layout == "1T_FarmConners"
+        lyt     = lyt_FC_1T;
+        t_id    = ones(Int8,1);
+        turb    = InnWind;
+    elseif layout == "3T_FarmConners"
+        lyt     = lyt_FC_3T;
+        t_id    = ones(Int8,3);
+        turb    = InnWind;
+    elseif layout == "9T_FarmConners"
+        lyt     = lyt_FC_9T;
+        t_id    = ones(Int8,9);
+        turb    = InnWind;
+    elseif layout == "1T_DTU10MW"
+        lyt     = lyt_1T;
+        t_id    = ones(Int8,3);
+        turb    = DTU10MW;
+    elseif layout == "2T_DTU10MW"
+        lyt     = lyt_2T;
+        t_id    = ones(Int8,2);
+        turb    = DTU10MW;
+    elseif layout == "3T_DTU10MW"
+        lyt     = lyt_3T;
+        t_id    = ones(Int8,3);
+        turb    = DTU10MW;
+    elseif layout == "9T_DTU10MW"
+        lyt     = lyt_9T;
+        t_id    = ones(Int8,9);
+        turb    = DTU10MW;
+    else
+        error("Unknown layout, please choose a known name.")
     end
-    return -1;
+
+    #   determine nT
+    nT = length(lyt)
+    #   Create turbine array
+    turbines = [];
+    for iT = 1:nT
+        push!(turbines,
+            Turbine(
+                lyt[iT],    # x
+                lyt[iT+1],  # y
+                turb[1],    # z
+                t_id[iT],   # ttype
+                zeros(nOP), # Ct
+                zeros(nOP), # γ
+                zeros(1),   # Cp
+                zeros(nOP,nC) # op_x
+                zeros(nOP,nC) # op_y
+                zeros(nOP,nC) # op_z
+                zeros(nOP,nC) # op_x1
+                zeros(nOP,nC) # op_y1
+                zeros(nOP,nC) # op_u
+                zeros(nOP,nC) # op_φ
+                zeros(nOP,nC) # op_I0
+            );
+    end
+
+    cConst = generateChainConstants(nC);
+    cnst = Constants(
+            turb[2:4:end],  # t_D
+            turb[3:4:end],  # t_η
+            turb[4:4:end],  # t_p_p
+            cConst[:,1],    # c_w
+            cConst[:,2],    # c_νy
+            cConst[:,3]);   # c_νz
+    # Init E
+    e = Ensemble(turbines);
+    return e;
 end
 
-function allocOPStruct(nE,nT,nC,nOP)
-    op = OP(
-        zeros(nE*nT*nC*nOP),    # x
-        zeros(nE*nT*nC*nOP),    # y
-        zeros(nE*nT*nC*nOP),    # z
-        zeros(nE*nT*nC*nOP),    # x1
-        zeros(nE*nT*nC*nOP),    # y1
-        zeros(nE*nT*nC*nOP),    # u
-        zeros(nE*nT*nC*nOP),    # φ
-        zeros(nE*nT*nC*nOP));    # i0
-    return op
+function generateChainConstants(nC)
+    body
 end
-
-function allocChainStruct(nE,nT,nC,nOP)
-    c = C(
-        zeros(nC),              # c_w
-        zeros(nC),              # c_νy
-        zeros(nC),              # c_νz
-        1:nOP:(nE*nT*nC*nOP-nOP+1) # pntr_c2op
-    );
-    return c
-end
-
-function allocEnsembleStruct(nE,nT,nC)
-    e = E(1:nT*nC:(nE-1)*nT*nC+1);
-    return e
-end
-
-#=
-function allocSimStruct()
-    sim = Sim(
-        0.0,    # t_end
-        0.0,    # Δt
-        0,      # nt
-        0.0,    # w
-        0.0,    # ρ
-        0.0);   # α_s
-    return sim
-end
-
-function allocControl()
-    control = Control("",false);
-    return control
-end
-=#
-# Ct(λ,β) & Cp(λ,β) look up tables
-
-
-# Ct(u) & Cp(u) look up tables
 
 end  # module FLORIDyn_InitStructs
